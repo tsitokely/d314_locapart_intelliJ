4e78ca9fea0d9b8942c38b87d1085e4e6bb30741
Tsitohaina Rakotondramasy <7864009+tsitokely@users.noreply.github.com>
2024-04-16 00:28:25 -0400

    x

--- src/main/java/resources/CityResource.java
+++ src/main/java/resources/CityResource.java
@@ -2,7 +2,14 @@ package resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Context;
+import DAO.CityDAO;
+import entity.City;
 
 /**
  *
@@ -12,9 +19,10 @@ import jakarta.ws.rs.core.Response;
 public class CityResource {
     
     @GET
-    public Response ping(){
-        return Response
-                .ok("ping Jakarta EE")
+    @Produces("application/json")
+    public Response ping(@Context jakarta.ws.rs.core.UriInfo uri){
+            return Response
+                .ok("City",MediaType.TEXT_PLAIN)
                 .build();
     }
 }
