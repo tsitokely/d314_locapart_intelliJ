1d4d27c15f1235406b1d002bf17316e984bb38e8
Tsitohaina Rakotondramasy <tsitohaina.toetra.rak@outlook.com>
2024-04-15 21:57:07 -0400

    initial commit

---
+++ src/main/java/resources/CityResource.java
@@ -0,0 +1,20 @@
+package resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("WsLocaPart")
+public class CityResource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
