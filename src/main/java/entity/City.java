f24ede1ecd93ed98ba226b702b0be45f5d707507
Tsitohaina Rakotondramasy <7864009+tsitokely@users.noreply.github.com>
2024-04-15 23:38:28 -0400

    Added city model

---
+++ src/main/java/entity/City.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author tsito
+ */
+
+@XmlRootElement(name="City")
+public class City {
+    private String cityID="";
+    private String cityName="";
+    
+    public City(){}
+
+    @XmlElement(name="cityID",required=true,nillable=false)
+    public String getCityID() {
+        return cityID;
+    }
+
+    @XmlElement(name="cityName",required=true,nillable=false)
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityID(String cityID) {
+        this.cityID = cityID;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
